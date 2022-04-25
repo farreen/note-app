e514d2d047d2bf2131641865e37e56f39eb01ac7
Farah Nasreen <farahnasreen2000@gmail.com>
2022-04-25 23:16:21 +0530

    Add new file and edit note

---
+++ src/components/Markdown.jsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import MDEditor from '@uiw/react-md-editor';
+
+function Markdown() {
+  const [title, setTitle] = React.useState();
+  const [content, setContent] = React.useState();
+  const [noteList, setnoteList] = React.useState([]);
+  const [selectedTitle, setSelectedTitle] = React.useState(null);
+  const [displayContent, setDisplayContent] = React.useState(null);  
+  const [id, setId] = React.useState();
+  const [selectedNote, setSelectedNote] = React.useState(null);  
+  const [editing, setEditing] = React.useState(false);  
+
+  const getNote = () => {
+    fetch('http://localhost:8080/api/get')
+    .then((res) => {
+      if(res.ok) {
+        console.log(res);
+        return res.text();
+      }
+    })
+    .then(text => {
+      console.log(text);
+      setContent(text);
+    })
+  }
+
+  const getListOfNote = () => {
+    fetch('http://localhost:8080/api/list')
+    .then(res => {
+      if(res.ok){
+        return res.json();
+      }
+    })
+    .then(list => {
+      console.log('list', list);  
+      setnoteList(list);
+    })
+  }
+  React.useEffect(getNote, [])
+  React.useEffect(getListOfNote, [])
+    
+  const saveNote = () => {
+    const newValue = {title, content}; 
+    fetch('http://localhost:8080/api/insert',{
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(newValue)    
+    })
+    .then(() => {
+     console.log(newValue);
+    });
+  }
+
+  const updateNote = () => {
+    const newValue = {id, title, content};
+    fetch('http://localhost:8080/api/update',{
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: newValue
+    })
+    .then(() => {
+      console.log('newValue',newValue);
+    });  
+  }
+
+  return (
+    <div>
+      <div style={{width: 200, backgroundColor: 'lightgray'}}>
+        <ul>
+          {noteList.map((note, index) => (
+            <li>    
+              <a href="javascript:void 0" onClick={() => setSelectedNote(note)}>{note.title}</a>
+            </li>  
+          ))}
+        </ul>
+      </div>
+
+      {selectedNote !== null ? editing === false ? <div> 
+        <MDEditor.Markdown source={selectedNote.id} />
+        <MDEditor.Markdown source={selectedNote.title} style={{fontSize: "24pt", fontWeight: "bold"}} />
+        <MDEditor.Markdown source={selectedNote.content} />
+        <button onClick={() => setEditing(true)}>Edit</button>
+      </div> :<span>Editing</span>: null} 
+
+      <div style={{marginLeft: 200}}>
+        <input
+          type = "text"
+          onChange={({target: {value}}) => setTitle(value)}
+        />
+      </div>
+      <div style={{marginLeft: 200}}>
+        <MDEditor
+          value={content}
+          onChange={setContent}
+        />
+      </div>
+      <div style={{marginLeft: 200}}>
+        <button onClick={saveNote}>Save note</button>
+      </div>
+    </div>
+  );
+}
+
+export default Markdown; 
