f3c21942e864272450fd71254471d3deeede0b0b
Farah Nasreen <farahnasreen2000@gmail.com>
2022-05-22 19:35:29 +0530

    Refactor the code, extracting two components NoteReadOnly and NoteEditable

--- src/components/Markdown.jsx
+++ src/components/Markdown.jsx
@@ -1,6 +1,34 @@
 import React from 'react';
 import MDEditor from '@uiw/react-md-editor';
 
+const NoteReadOnly = ({selectedNote, setEditing}) => {
+  return ( 
+    <div>
+      <MDEditor.Markdown source={selectedNote.id} />
+      <MDEditor.Markdown source={selectedNote.title} style={{fontSize: "24pt", fontWeight: "bold"}} />
+      <MDEditor.Markdown source={selectedNote.content} />
+      <button onClick={() => setEditing(true)}>Edit</button>
+    </div>
+  )
+}
+  
+const NoteEditable = ({selectedNote, setSelectedNote, updateNote}) => {
+  return (
+    <div>    
+      <input type="text" 
+        value={selectedNote.title} 
+        onChange={(e) => setSelectedNote({...selectedNote, title: e.target.value})}
+      />
+      <MDEditor 
+        value={selectedNote.content}
+        onChange={(newContent) => setSelectedNote({...selectedNote, content: newContent})}
+      />
+      <button onClick={updateNote}>update</button>
+    </div>
+  )
+}
+
+
 function Markdown() {
   const [title, setTitle] = React.useState();
   const [content, setContent] = React.useState();
@@ -63,7 +91,7 @@ function Markdown() {
       console.log(res);  
     })
   }
-
+    
   return (
     <div>
       <div style={{width: 200, backgroundColor: 'lightgray'}}>
@@ -76,23 +104,10 @@ function Markdown() {
         </ul>
       </div>
 
-      {selectedNote !== null ? editing === false ? <div> 
-        <MDEditor.Markdown source={selectedNote.id} />
-        <MDEditor.Markdown source={selectedNote.title} style={{fontSize: "24pt", fontWeight: "bold"}} />
-        <MDEditor.Markdown source={selectedNote.content} />
-        <button onClick={() => setEditing(true)}>Edit</button>
-        </div> :<div>
-          <input type="text" 
-            value={selectedNote.title} 
-            onChange={(e) => setSelectedNote({...selectedNote, title: e.target.value})}
-          />
-          <MDEditor 
-            value={selectedNote.content}
-            onChange={(newContent) => setSelectedNote({...selectedNote, content: newContent})}
-          />
-          <button onClick={updateNote}>update</button>
-        </div>
-      : null} 
+      {selectedNote !== null? editing === false? 
+        <NoteReadOnly selectedNote={selectedNote} setEditing={setEditing} /> :
+        <NoteEditable selectedNote={selectedNote} setSelectedNote={setSelectedNote} updateNote={updateNote} />
+      :null}
 
       <div style={{marginLeft: 200}}>
         <input
